a711cc76b826165e4f900e6c317a301f7523bb14
Christian Paun <christianpaun@gmail.com>
2018-03-09 20:32:26 +0000

    adding form validation {server side} and getting user info with express/body-parser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -17,11 +19,23 @@ console.log('MongoDB Connected')
 .catch((error)=>{
 console.log('error')
 })
+//Load idea Model
 
+require('./models/Idea');
+
+const Idea = mongoose.model('ideas');
 //handlebars middleware
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+//body-parser middleware
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
 
 //Index Route
 
@@ -36,6 +50,36 @@ res.render('index', {
 
 app.get('/about', (req, res)=>{
 res.render('about')    
+});
+
+//Add idea form
+
+app.get('/ideas/add', (req, res)=>{
+    res.render('ideas/add')    
+    });
+
+//process form
+
+app.post('/ideas', (req, res)=>{
+    let errors = [];
+    if(!req.body.title){
+        errors.push({text:'Please add a title!'});   
+    }
+    if(!req.body.details){
+        errors.push({text:'Please add some details!'});   
+    }
+
+    if (errors.length>0){
+//re-render and pass back {errors & what the user has already entered}
+        res.render('ideas/add',{
+    errors: errors,
+    title:req.body.title,
+    details:req.body.details
+});
+    } else {
+        res.send('passed')
+    }
+
 });
 
 const port = 5000;
